3db0ca4813819c5be357c1820957025c7b3db719
devruji <en.rujikorn@gmail.com>
2022-10-09 13:51:09 +0700

    revise result display

--- runner.py
+++ runner.py
@@ -4,4 +4,4 @@ from quizzes._26 import Solution
 if __name__ == "__main__":
     s = Solution()
     k, nums = s.remove_duplicated_from_sorted_array([1, 1, 2, 2, 2])
-    print(k, nums)
+    print(f"k={k}", "|", f"nums={nums}")
