0bcdb799eeef84069bbfd4f0d76af4017b500225
Aimamorn Suvichakorn <Aimamorn.Suvichakorn@wdc.com>
2022-10-09 14:40:43 +0700

    initial commit quiz#01

--- runner.py
+++ runner.py
@@ -1,6 +1,12 @@
-from quizzes._169 import Solution
+from quizzes._1 import Solution as _1
+from quizzes._169 import Solution as _169
+
+if __name__ == "__main__":
+    input = [2, 2, 1, 1, 1, 2, 2]
+    print(f'quiz#169 (Majority Element): {input}, answer: {_169().majorityElement(input)}')
 
 
 if __name__ == "__main__":
-    s = Solution()
-    print(s.majorityElement([2, 2, 1, 1, 1, 2, 2]))
+    input = [-3, 0, 1, 3, 4]
+    target = 0
+    print(f'quiz#1 (TwoSum): input={input}, target ={target}, answer: {_1().twoSum(input, target)}')
